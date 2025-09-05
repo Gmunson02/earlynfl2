35ae13b0ee3974ae38f532f349f0fd6395ec988e
gmunson01@gmail.com <gmunson01@gmail.com>
2025-09-05 14:12:10 -0400

    disable optimization

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    unoptimized: true, // 🚨 Kill switch: disables all next/image optimizations
     remotePatterns: [
       {
         protocol: "https",
